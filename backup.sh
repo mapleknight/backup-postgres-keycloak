701e79afcae6d779aa67d495071a6fd09511d2c0
sai sunil <47606773+saisunil2303@users.noreply.github.com>
2019-05-07 10:44:11 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,11 +2,6 @@
 pod=$(/google/google-cloud-sdk/bin/kubectl get po -n keycloak | grep postgres | awk '{print $1}')
 gcloud container clusters get-credentials kube-dev --zone us-central1-a --project keycloak-test-230419
 /google/google-cloud-sdk/bin/kubectl exec "$pod" -n keycloak -- pg_dump -U keycloak > /home/sai/backup/postgresql_"$(date +%d-%m-%Y"_"%H_%M_%S)".sql
-if [[ $? -eq 0 ]]; then
-  echo "success " >> /home/sai/backup/backup.log
-else
-  echo "failed" >> /home/sai/backup/backup.log
-fi
 /google/google-cloud-sdk/bin/gsutil cp /home/sai/backup/postgresql_* gs://backups-keycloak/Backups/
 if [[ $? -eq 0 ]]; then
   echo "$(date +%d-%m-%Y"_"%H_%M_%S) Backup transfer successful" >> /home/sai/backup/backup.log
